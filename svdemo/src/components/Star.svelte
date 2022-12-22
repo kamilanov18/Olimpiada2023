6b7f4b65aec8f2c616ef15ceafe56a98fa341635
kamilanov18 <kamilanov18@codingburgas.bg>
2022-12-22 22:25:17 +0200

    Add star component

---
+++ svdemo/src/components/Star.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+    export let radius:number;
+    export let pos: import('svelte-cubed/types/common').Position;
+    let rightAscension=0;
+    let declination=0;
+    let distance=0;
+</script>
+
+<SC.Mesh position={pos} geometry={new THREE.SphereGeometry(radius)}/>
+
+<!--<SC.Mesh position={pos} geometry={new THREE.SphereGeometry(radius)}/>-->
--- svdemo/src/routes/+page.svelte
+++ svdemo/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import * as THREE from 'three';
+	import * as SC from 'svelte-cubed';
+	import Star from './../components/Star.svelte';
+</script>
+
+<SC.Canvas>
+    <Star pos={[1,1,1]} radius={0.05}/>
+    <Star pos={[1,2,1]} radius={0.05}/>
+    <Star pos={[1,3,1]} radius={0.05}/>
+    <Star pos={[1,4,1]} radius={0.05}/>
+	<SC.PerspectiveCamera position={[1, 1, 1]} />
+    <SC.OrbitControls></SC.OrbitControls>
+</SC.Canvas>
