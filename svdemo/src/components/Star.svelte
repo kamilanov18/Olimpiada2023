33821023ba5ad10d36676ff61bd7f6f5f544bc8f
kamilanov18 <kamilanov18@codingburgas.bg>
2022-12-25 17:21:09 +0200

    Input Ursa Major main star coordinates and 3d space formula

--- svdemo/src/components/Star.svelte
+++ svdemo/src/components/Star.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
     import * as THREE from 'three';
 	import * as SC from 'svelte-cubed';
+	import type { Position } from 'svelte-cubed/types/common';
+
     export let radius:number;
-    export let pos: import('svelte-cubed/types/common').Position;
-    let rightAscension=0;
-    let declination=0;
-    let distance=0;
+    export let rightAscension: number=0; // lat
+    export let declination: number=0;    // lon
+    export let distance: number=0;
+    //export let name: string;
+
+    function get3DCoordinates(flattening:1|0=0):Position {
+        const celestialRadius = 110;
+        let ls = Math.atan((1-flattening)**2 * Math.tan(rightAscension))
+        let x = celestialRadius * Math.cos(ls) * Math.cos(declination) + distance * Math.cos(rightAscension) * Math.cos(declination);
+        let y = celestialRadius * Math.cos(ls) * Math.sin(declination) + distance * Math.cos(rightAscension) * Math.sin(declination);
+        let z = celestialRadius * Math.sin(ls) + distance * Math.sin(rightAscension);
+        return [x,y,z];
+    }
 </script>
 
-<SC.Mesh position={pos} geometry={new THREE.SphereGeometry(radius)}/>
-
-<!--<SC.Mesh position={pos} geometry={new THREE.SphereGeometry(radius)}/>-->
+<SC.Mesh position={get3DCoordinates()} geometry={new THREE.SphereGeometry(radius)}/>
--- svdemo/src/routes/+page.svelte
+++ svdemo/src/routes/+page.svelte
@@ -5,10 +5,15 @@
 </script>
 
 <SC.Canvas>
-    <Star pos={[1,1,1]} radius={0.05}/>
-    <Star pos={[1,2,1]} radius={0.05}/>
-    <Star pos={[1,3,1]} radius={0.05}/>
-    <Star pos={[1,4,1]} radius={0.05}/>
+    <!--<SC.Mesh material={new THREE.MeshStandardMaterial({color: 0xffffff,emissive:0xffffff})} geometry={new THREE.SphereGeometry(110)} position={[0,0,0]}></SC.Mesh>-->
+    
+    <Star rightAscension={11.0621778} declination={55.9598333} distance={81} radius={1}/> <!--Dubhe 81-->
+    <Star rightAscension={11.0306639} declination={56.3823333} distance={79} radius={1}/> <!--Merak 79-->
+    <Star rightAscension={11.8971500} declination={53.6947222} distance={84} radius={1}/> <!--Phecda 84-->
+    <Star rightAscension={12.2570694} declination={57.0326111} distance={81} radius={1}/> <!--Megrez 81-->
+    <Star rightAscension={12.9004528} declination={55.9598333} distance={81} radius={1}/> <!--Alioth 81-->
+    <Star rightAscension={13.3989806} declination={54.9218222} distance={83} radius={1}/> <!--Mizar 83-->
+    <Star rightAscension={13.7923750} declination={49.3133056} distance={101} radius={1}/> <!--Alkaid 101-->
 	<SC.PerspectiveCamera position={[1, 1, 1]} />
     <SC.OrbitControls></SC.OrbitControls>
 </SC.Canvas>
