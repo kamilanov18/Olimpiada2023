d8ae566e96117713166d8c20992560eeca1f7a3a
kamilanov18 <kamilanov18@codingburgas.bg>
2023-02-01 12:26:27 +0200

    Begin HTML onHover integration

--- svdemo/src/components/Star.svelte
+++ svdemo/src/components/Star.svelte
@@ -1,32 +1,26 @@
 <script lang="ts">
-    import * as THREE from 'three';
-	import { T, type ThreltePointerEvent } from '@threlte/core';
+	import { Instance, T, type Position, type ThreltePointerEvent } from '@threlte/core';
+	import { degToRad } from 'three/src/math/MathUtils';
 
-    export let radius:number;
+    export let id:number;
     export let rightAscension: number; // lat
     export let declination: number;    // lon
     export let distance: number;
+    let coordinatesIn3DSpace = get3DCoordinates();
 
-    const spawnHTML = (e: CustomEvent<ThreltePointerEvent>) => {
-        console.log(e);
+    const onPointerEnter = (e: CustomEvent<ThreltePointerEvent>) => {
+        const distanceToMesh = e.detail.distance;
+        console.log("test: "+distanceToMesh);
     }
 
-    function Radians(degrees: number)
-    {
-        return degrees*Math.PI/180;
-    }
-
-    function get3DCoordinates(): [number,number,number] {
-        let ra = Radians(rightAscension);
-        let dec = Radians(declination);
-        // let x = distance * Math.sin(ec) * Math.cos(ra);
-        // let y = distance * Math.sin(dec) * Math.sin(ra);
-        // let z = distance * Math.cos(dec) * Math.sign(dec);
+    function get3DCoordinates(): Position/*[number,number,number]*/ {
+        let ra = degToRad(rightAscension);
+        let dec = degToRad(declination);
         let x = distance* Math.cos(dec)*Math.cos(ra);
         let y = distance*Math.cos(dec)*Math.sin(ra);
         let z = distance*Math.sin(dec);
-        return [x,y,z];
+        return {x,y,z};
     }
 </script>
 
-<T.Mesh  material={new THREE.MeshStandardMaterial({color: 0xffffff,emissive:0xffffff})} position={get3DCoordinates()} geometry={new THREE.SphereGeometry(radius)}/>
+<Instance id={id.toString()} on:pointerenter={onPointerEnter} color={0xffffff} position={coordinatesIn3DSpace} />
--- svdemo/src/routes/+page.svelte
+++ svdemo/src/routes/+page.svelte
@@ -1,16 +1,21 @@
 <script lang='ts'>
 	import * as THREE from 'three';
-	import { Canvas, OrbitControls, T } from '@threlte/core';
-    import { Environment } from '@threlte/extras'
+	import { Canvas, Line2, OrbitControls, type PerspectiveCamera, T, Instance } from '@threlte/core';
+    import { Environment, HTML } from '@threlte/extras'
     import Star from '../components/Star.svelte';
     import type { PageData } from './$types';
+	// import TrackBallControls from 'src/components/TrackBallControls.svelte';
+	import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
 
     export let data: PageData;
     const stars = data.stars.data;
+
+    let hoverElement;
+    let camera:PerspectiveCamera;
 </script>
 
 <style>
-    div {
+    .canvas-container {
       position: fixed;
 	  top: 0;
 	  left: 0;
@@ -20,19 +25,37 @@
 </style>
 
 <div class="canvas-container">
-<Canvas>
+<Canvas >
+  <HTML bind:this={hoverElement} transform position={{x:0,y:0,z:0}}>
+    <span style="background-color:red; color:white;">A</span>
+  </HTML>
     <T.GridHelper />
         <Environment path ='./' files={'black_background.png'} isBackground={true} />
         <T.PerspectiveCamera makeDefault near={0.001} position={[1,1,1]}>
-            <OrbitControls enableDamping dampingFactor={0.09} />
+            <OrbitControls keys={{LEFT: 'a',RIGHT:'d',UP:'w',BOTTOM:'s'}} zoomSpeed={10} enableDamping dampingFactor={0.09} />
         </T.PerspectiveCamera>
+
+        <Line2
+          points={[     
+            [0,0,0],
+            [0, 2, 1],
+            [-1, 1, 4]
+          ]}
+          material={new LineMaterial({
+            worldUnits: true,
+            linewidth:0.00007
+          })}
+        />
 
         <T.Mesh material={new THREE.MeshStandardMaterial({color: 0xffffff,emissive:0xffffff})} geometry={new THREE.SphereGeometry(0.001)} position={[0,0,0]} />
         <T.Mesh material={new THREE.MeshStandardMaterial({color: 0xfff00,emissive:0xffff00})} geometry={new THREE.SphereGeometry(0.1)} position={[0,0,1]} />
 
-        {#each stars as star}
-            {console.log(star)}
-            <Star rightAscension={star[1]} declination={star[2]} distance={((1/star[3]))*3.26} radius={1e-4}></Star>
-        {/each}
+        <T.InstancedMesh id="stars" interactive material={new THREE.MeshStandardMaterial({color: 0xffffff,emissive:0xffffff})} geometry={new THREE.SphereGeometry(1e-4)}>
+            {#each stars as star}
+                {console.log(star)}
+                <Star id={star[0]} rightAscension={star[1]} declination={star[2]} distance={((1/star[3]))*3.26} />
+            {/each}
+            <Instance />
+        </T.InstancedMesh>
 </Canvas>
 </div>
