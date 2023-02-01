ed121fc3253d49e4a4a00672a40ae538d9557f3f
kamilanov18 <kamilanov18@codingburgas.bg>
2023-02-01 20:26:54 +0200

    Try to fix cursor reactivity

--- svdemo/src/components/Star.svelte
+++ svdemo/src/components/Star.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
 	import { Instance, T, type Position, type ThreltePointerEvent } from '@threlte/core';
 	import type { StarData } from 'src/types';
+	import type { Object3D } from 'three';
 
     export let starData: StarData;
+    export let cursorTarget:Object3D
 
     const onPointerEnter = (e: CustomEvent<ThreltePointerEvent>) => {
-        const distanceToMesh = e.detail.distance;
-        console.log("test: "+distanceToMesh);
+        console.log("vutre sum");
+        console.log("before: "+cursorTarget.position.x);
+        if(starData.coordinates.x&&starData.coordinates.y&&starData.coordinates.z)
+            cursorTarget.position.set(starData.coordinates.x,starData.coordinates.y,starData.coordinates.z)
+        console.log("after: "+cursorTarget.position.x);
+        cursorTarget=cursorTarget;
     }
 </script>
 
--- svdemo/src/routes/+page.svelte
+++ svdemo/src/routes/+page.svelte
@@ -1,13 +1,8 @@
 <script lang='ts'>
-	import * as THREE from 'three';
-	import { Canvas, OrbitControls, type PerspectiveCamera, T, InstancedMesh } from '@threlte/core';
-  import { Environment, HTML } from '@threlte/extras'
-  import Star from '../components/Star.svelte';
+	import { Canvas } from '@threlte/core';
+	import Scene from '../components/Scene.svelte';
   import type { PageData } from './$types';
-	import type { StarData } from 'src/types';
-
-    export let data: PageData;
-    const stars: StarData[] = data.stars;
+  export let data: PageData;
 </script>
 
 <style>
@@ -21,23 +16,7 @@
 </style>
 
 <div class="canvas-container">
-<Canvas >
-  <!-- <HTML transform position={{x:0,y:0,z:0}}>
-    <img src="../static/cursor.png">
-  </HTML> -->
-    <T.GridHelper />
-        <Environment path ='./' files={'black_background.png'} isBackground={true} />
-        <T.PerspectiveCamera makeDefault near={0.001} position={[1,1,1]}>
-            <OrbitControls keys={{LEFT: 'a',RIGHT:'d',UP:'w',BOTTOM:'s'}} zoomSpeed={10} enableDamping dampingFactor={0.09} />
-        </T.PerspectiveCamera>
-
-        <T.Mesh material={new THREE.MeshStandardMaterial({color: 0xffffff,emissive:0xffffff})} geometry={new THREE.SphereGeometry(0.001)} position={[0,0,0]} />
-        <T.Mesh material={new THREE.MeshStandardMaterial({color: 0xfff00,emissive:0xffff00})} geometry={new THREE.SphereGeometry(0.1)} position={[0,0,1]} />
-
-        <InstancedMesh interactive material={new THREE.MeshStandardMaterial({color: 0xffffff,emissive:0xffffff})} geometry={new THREE.SphereGeometry(1e-4)}>
-            {#each stars as star}
-                <Star starData={star} />
-            {/each}
-        </InstancedMesh>
-</Canvas>
+  <Canvas >
+    <Scene data={data} />
+  </Canvas>
 </div>
