afd2a30b999a25fa457108a02b1866ec5155ef53
Martin Atanasov <mvatanasov18@codingburgas.bg>
2023-02-02 23:02:37 +0200

    Add Navbar

---
+++ svdemo/src/components/NavBar.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import {
+      Collapse,
+      Navbar,
+      NavbarToggler,
+      NavbarBrand,
+      Nav,
+      NavItem,
+      NavLink,
+      Dropdown,
+      DropdownToggle,
+      DropdownMenu,
+      DropdownItem,
+      Styles
+    } from 'sveltestrap/src';
+  
+    let isOpen = false;
+  
+    function handleUpdate(event:any) {
+      isOpen = event.detail.isOpen;
+    }
+  </script>
+
+<style>
+    *{
+        box-sizing: border-box;
+        color:aliceblue;
+        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    }
+    </style>
+
+  <Styles />
+  <Navbar color="dark" dark expand="md">
+    <NavbarBrand href="/index">StarIndex</NavbarBrand>
+    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+      <Nav class="ms-auto" navbar>
+        <NavItem>
+          <NavLink href="https://github.com/kamilanov18/Olimpiada2023">GitHub</NavLink>
+        </NavItem>
+      </Nav>
+    </Collapse>
+  </Navbar>
---
+++ svdemo/src/routes/index/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import NavBar from "../../components/NavBar.svelte";
+</script>
+<style>
+*{
+    box-sizing: border-box;
+    color:aliceblue;
+    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+:global(body){
+    background: linear-gradient(176deg,rgb(18,24,27) 50%,rgb(32,39,55) 100%);;
+    background-attachment: fixed;
+}
+</style>
+
+
+    <NavBar></NavBar>
+<h1>Home page</h1>
