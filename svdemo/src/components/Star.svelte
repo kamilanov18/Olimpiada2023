9be6db9f41cce29e62a08d6e105f6642c55e67ef
kamilanov18 <kamilanov18@codingburgas.bg>
2023-02-02 17:17:24 +0200

    Fix Pointer hoverability issues and camera changes * Add stores.ts as a container for stores, usable by all components * Add stores for the position and visibility for the cursor and the star which has been clicked on * Add camera center functionality, now it faces a clicked star Closes #1 Closes #13

--- svdemo/src/components/Star.svelte
+++ svdemo/src/components/Star.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
-	import { Instance, type ThreltePointerEvent } from '@threlte/core';
+	import { Instance, type ThreltePointerEvent, useThrelte } from '@threlte/core';
 	import type { StarData } from 'src/types';
-    import { cursorPosition } from '../stores'
+    import { cursorPosition, isCursorVisible, targetStar } from '../stores'
     export let starData: StarData;
 
-
     const onPointerEnter = (e: CustomEvent<ThreltePointerEvent>) => {
+        console.log(e)
         console.log("vutre sum");
         cursorPosition.set(starData.coordinates);
+        isCursorVisible.set(true);
+    }
+
+    const onPointerLeave = (e: CustomEvent<ThreltePointerEvent>) => {
+
+        console.log("navun sum");
+        isCursorVisible.set(false);
+    }
+
+    const onClick = (e: CustomEvent<ThreltePointerEvent>) => {
+        console.log("kliknah");
+        targetStar.set(starData);
     }
 </script>
 
-<Instance on:pointerenter={onPointerEnter} color={0xffffff} position={starData.coordinates} />
+<Instance on:pointerleave={onPointerLeave} on:pointerenter={onPointerEnter} on:click={onClick}  color={0xffffff} position={starData.coordinates} />
--- svdemo/src/routes/+page.server.ts
+++ svdemo/src/routes/+page.server.ts
@@ -17,7 +17,7 @@ function get3DCoordinates(rightAscension: number,declination: number,parallax: n
 }
 
 export const load =  ( async () => {
-  const res = await fetch("https://gea.esac.esa.int/tap-server/tap/sync?REQUEST=doQuery&LANG=ADQL&FORMAT=json&QUERY=SELECT+TOP+1000+source_id,ra,dec,parallax+FROM+gaiadr3.gaia_source+WHERE+parallax>0.1+ORDER+BY+parallax+DESC", {
+  const res = await fetch("https://gea.esac.esa.int/tap-server/tap/sync?REQUEST=doQuery&LANG=ADQL&FORMAT=json&QUERY=SELECT+TOP+10+source_id,ra,dec,parallax+FROM+gaiadr3.gaia_source+WHERE+parallax>0.1+ORDER+BY+parallax+DESC", {
     method: 'GET',
     headers: {
       'content-type': 'application/json'
