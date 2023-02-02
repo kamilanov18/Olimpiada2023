9a0740cd78a06a38711bb751e7255020d98ea61a
Martin Atanasov <mvatanasov18@codingburgas.bg>
2023-02-03 00:36:58 +0200

    Add example modal for login

--- svdemo/src/components/NavBar.svelte
+++ svdemo/src/components/NavBar.svelte
@@ -1,43 +1,81 @@
 <script lang="ts">
-    import {
-      Collapse,
-      Navbar,
-      NavbarToggler,
-      NavbarBrand,
-      Nav,
-      NavItem,
-      NavLink,
-      Dropdown,
-      DropdownToggle,
-      DropdownMenu,
-      DropdownItem,
-      Styles
-    } from 'sveltestrap/src';
-  
-    let isOpen = false;
-  
-    function handleUpdate(event:any) {
-      isOpen = event.detail.isOpen;
-    }
-  </script>
+  import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    Styles,
+    Button,
+      Modal,
+      ModalBody,
+      ModalFooter,
+      ModalHeader
+  } from 'sveltestrap/src';
 
+  export let open = false;
+    
+    const toggle = () => {
+      open = !open;
+    };
+
+  let isOpen = false;
+  let isLoggedIn = false;
+
+  function handleUpdate(event:any) {
+    isOpen = event.detail.isOpen;
+  }
+
+  function toggleLogin() {
+    isLoggedIn = !isLoggedIn;
+  }
+</script>
 <style>
-    *{
-        box-sizing: border-box;
-        color:aliceblue;
-        font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    }
-    </style>
+  :global(*){
+      color:aliceblue;
+      font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+  }
+  :global(div.modal-content){
+    background-color: black !important;
+  }
+  </style>
+<Styles />
+<Navbar color="dark" dark expand="md">
+  <NavbarBrand href="/index">StarIndex</NavbarBrand>
+  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
+  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+    <Nav class="ms-auto" navbar>
+      <NavItem>
+        <NavLink href="https://github.com/kamilanov18/Olimpiada2023">GitHub</NavLink>
+      </NavItem>
+    </Nav>
+    <Nav navbar class="ml-auto">
+      {#if !isLoggedIn}
 
-  <Styles />
-  <Navbar color="dark" dark expand="md">
-    <NavbarBrand href="/index">StarIndex</NavbarBrand>
-    <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-    <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-      <Nav class="ms-auto" navbar>
+
+        <div>
+          <Button color="light" outline on:click={toggle}>Login</Button>
+          <Modal isOpen={open} toggle={toggle}>
+            <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+            <ModalBody>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua.
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" on:click={toggleLogin}>Do Something</Button>
+              <Button color="secondary" on:click={toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
+
+
+      {:else}
         <NavItem>
-          <NavLink href="https://github.com/kamilanov18/Olimpiada2023">GitHub</NavLink>
+          <NavLink href="#" on:click={toggleLogin}>Logout</NavLink>
         </NavItem>
-      </Nav>
-    </Collapse>
-  </Navbar>
+      {/if}
+    </Nav>
+  </Collapse>
+</Navbar>
--- svdemo/src/routes/index/+page.svelte
+++ svdemo/src/routes/index/+page.svelte
@@ -2,11 +2,7 @@
     import NavBar from "../../components/NavBar.svelte";
 </script>
 <style>
-*{
-    box-sizing: border-box;
-    color:aliceblue;
-    font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-}
+
 :global(body){
     background: linear-gradient(176deg,rgb(18,24,27) 50%,rgb(32,39,55) 100%);;
     background-attachment: fixed;
