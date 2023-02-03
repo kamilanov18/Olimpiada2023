8686ffacee4afb72bc802f83b7321f7800e89fc0
Martin Atanasov <mvatanasov18@codingburgas.bg>
2023-02-03 13:40:52 +0200

    Chage login form

--- svdemo/src/components/NavBar.svelte
+++ svdemo/src/components/NavBar.svelte
@@ -1,81 +1,109 @@
 <script lang="ts">
-  import {
-    Collapse,
-    Navbar,
-    NavbarToggler,
-    NavbarBrand,
-    Nav,
-    NavItem,
-    NavLink,
-    Styles,
-    Button,
-      Modal,
-      ModalBody,
-      ModalFooter,
-      ModalHeader
-  } from 'sveltestrap/src';
+	import {
+		Collapse,
+		Navbar,
+		NavbarToggler,
+		NavbarBrand,
+		Nav,
+		NavItem,
+		NavLink,
+		Styles,
+		Button,
+		Modal,
+		ModalBody,
+		ModalFooter,
+		ModalHeader,
+		InputGroup,
+		InputGroupText,
+		Input,
+		Form
+	} from 'sveltestrap/src';
 
-  export let open = false;
-    
-    const toggle = () => {
-      open = !open;
-    };
+	export let open = false;
 
-  let isOpen = false;
-  let isLoggedIn = false;
+	const toggle = () => {
+		open = !open;
+	};
 
-  function handleUpdate(event:any) {
-    isOpen = event.detail.isOpen;
-  }
+	let isOpen = false;
+	let isLoggedIn = false;
 
-  function toggleLogin() {
-    isLoggedIn = !isLoggedIn;
-  }
+	function handleUpdate(event: any) {
+		isOpen = event.detail.isOpen;
+	}
+
+	function toggleLogin() {
+		isLoggedIn = !isLoggedIn;
+	}
 </script>
-<style>
-  :global(*){
-      color:aliceblue;
-      font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-  }
-  :global(div.modal-content){
-    background-color: black !important;
-  }
-  </style>
+
 <Styles />
 <Navbar color="dark" dark expand="md">
-  <NavbarBrand href="/index">StarIndex</NavbarBrand>
-  <NavbarToggler on:click={() => (isOpen = !isOpen)} />
-  <Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
-    <Nav class="ms-auto" navbar>
-      <NavItem>
-        <NavLink href="https://github.com/kamilanov18/Olimpiada2023">GitHub</NavLink>
-      </NavItem>
-    </Nav>
-    <Nav navbar class="ml-auto">
-      {#if !isLoggedIn}
-
-
-        <div>
-          <Button color="light" outline on:click={toggle}>Login</Button>
-          <Modal isOpen={open} toggle={toggle}>
-            <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-            <ModalBody>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua.
-            </ModalBody>
-            <ModalFooter>
-              <Button color="primary" on:click={toggleLogin}>Do Something</Button>
-              <Button color="secondary" on:click={toggle}>Cancel</Button>
-            </ModalFooter>
-          </Modal>
-        </div>
-
-
-      {:else}
-        <NavItem>
-          <NavLink href="#" on:click={toggleLogin}>Logout</NavLink>
-        </NavItem>
-      {/if}
-    </Nav>
-  </Collapse>
+	<NavbarBrand href="/index">StarIndex</NavbarBrand>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="ms-auto" navbar>
+			<NavItem>
+				<NavLink href="https://github.com/kamilanov18/Olimpiada2023">GitHub</NavLink>
+			</NavItem>
+		</Nav>
+		<Nav navbar class="ml-auto">
+			{#if !isLoggedIn}
+				<div>
+					<Button color="light" outline on:click={toggle}>Login</Button>
+					<Modal isOpen={open} {toggle}>
+						<ModalHeader {toggle}>Login form</ModalHeader>
+						<ModalBody>
+							<Form>
+								<InputGroup>
+									<InputGroupText>📧</InputGroupText>
+									<Input type="email" name="email" id="exampleEmail" placeholder="Email" />
+								</InputGroup>
+								<br />
+								<InputGroup>
+									<InputGroupText>@</InputGroupText>
+									<Input type="text" placeholder="Username" />
+								</InputGroup>
+                <br />
+								<InputGroup>
+									<InputGroupText>🔒</InputGroupText>
+									<Input
+										type="password"
+										name="password"
+										id="examplePassword"
+										placeholder="Password"
+									/>
+								</InputGroup>
+                <br/>
+                <InputGroup>
+                  
+									<Input style="border-radius: 5px;" type="text" placeholder="First Name" />
+                  <div style="width: 5%;"></div>
+                  <Input style="border-radius: 5px;" type="text" placeholder="Last Name" />
+                </InputGroup>
+							</Form>
+						</ModalBody>
+						<ModalFooter>
+							<Button color="primary" on:click={toggleLogin}>Submit</Button>
+							<Button color="secondary" on:click={toggle}>Cancel</Button>
+						</ModalFooter>
+					</Modal>
+				</div>
+			{:else}
+				<NavItem>
+					<NavLink href="#" on:click={toggleLogin}>Logout</NavLink>
+				</NavItem>
+			{/if}
+		</Nav>
+	</Collapse>
 </Navbar>
+
+<style>
+	:global(*) {
+		color: aliceblue;
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+	}
+	:global(div.modal-content) {
+		background-color: rgb(42, 46, 53) !important;
+	}
+</style>
