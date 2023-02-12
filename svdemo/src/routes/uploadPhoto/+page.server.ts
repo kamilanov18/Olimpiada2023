92302c7703bbdc3ebdd6009d89695f9b68b6a693
Martin Atanasov <mvatanasov18@codingburgas.bg>
2023-02-12 15:55:18 +0200

    Implement uploading photo to imgur

--- svdemo/src/routes/uploadPhoto/+page.server.ts
+++ svdemo/src/routes/uploadPhoto/+page.server.ts
@@ -1,7 +1,48 @@
-import {redirect} from '@sveltejs/kit'
-import type { PageServerLoad,Action, Actions } from './$types'
-export const load:PageServerLoad = async ({locals})=>{
-    if(!locals.user){
-        throw redirect(302,'login');
-    }
+import { redirect } from '@sveltejs/kit';
+import { ImgurClient } from 'imgur';
+import type { PageServerLoad, Action, Actions } from './$types';
+export const load: PageServerLoad = async ({ locals }) => {
+	if (!locals.user) {
+		throw redirect(302, 'login');
+	}
+};
+const client = new ImgurClient({
+	accessToken: process.env.TOKEN,
+	clientId: process.env.CLIENT_ID,
+	clientSecret: process.env.CLIENT_SECRET,
+	refreshToken: process.env.REFRESH_TOKEN
+});
+
+async function uploadToUmgur(file: File) {
+    const buffer = Buffer.from(await file.arrayBuffer());
+const base64 = buffer.toString('base64');
+	const response = await client.upload({
+		image:  base64,
+		type: 'base64'
+	});
+	console.log(response.data);
+	return response.data;
 }
+
+const upload: Action = async ({ request }) => {
+    try
+    {
+        if (request.method === 'POST') {
+		const form = await request.formData();
+		const image = form.get('img') as File;
+		console.log(form.get('img'));
+		if (image) {
+			console.log(`Received file with name: ${image.name}`);
+
+			return await uploadToUmgur(image);
+		}
+	}
+    }catch(e){
+        console.log(e);
+    }
+	
+};
+
+
+
+export const actions: Actions = { upload };
--- svdemo/src/routes/uploadPhoto/+page.svelte
+++ svdemo/src/routes/uploadPhoto/+page.svelte
@@ -1,60 +1,31 @@
 <script lang="ts">
 	import { Button } from 'sveltestrap/src';
-	let isSubmitClicked = false;
 
-	const changeState = () => {
-		isSubmitClicked = true;
-	};
-	const sendFileToImgur = (file: File) => {
-		const reader = new FileReader();
-		reader.readAsDataURL(file);
+	const dropHandler = (ev: DragEvent) => {
+		console.log('File(s) dropped');
 
-		reader.onload = () => {
-			if (typeof reader.result === 'string') {
-				const base64Data = reader.result.split(',')[1];
-                const formData = new FormData();
-                formData.append('image', base64Data);
-                formData.append('type', 'base64');
-                formData.append('client_id', '4617dcdecfebf68');
-                const options = {
-                    method: 'POST',
-                    headers: {
-                        'content-type': 'application/json',
-                        Authorization: 'Bearer c04a2c01ee2951ba49117b9ce192312171402a2b',
-                    },
-                    body: formData
-                };
-                fetch('https://api.imgur.com/3/upload', options)
-                    .then(response => response.json())
-                    .then(response => console.log(response.data.link))
-                    .catch(err => console.error(err));
-			}
-		};
-	};
-
-	const dropHandler = (ev: Event | undefined) => {
-		if (ev) {
-			console.log('File(s) dropped');
-
-			// Prevent default behavior (Prevent file from being opened)
-			(ev as DragEvent).preventDefault();
-
-			if ((ev as DragEvent).dataTransfer.items) {
+		// Prevent default behavior (Prevent file from being opened)
+		ev.preventDefault();
+		if (ev.dataTransfer) {
+			if (ev.dataTransfer.items) {
 				// Use DataTransferItemList interface to access the file(s)
-				[...(ev as DragEvent).dataTransfer.items].forEach((item, i) => {
+				[...ev.dataTransfer.items].forEach((item, i) => {
 					// If dropped items aren't files, reject them
 					if (item.kind === 'file') {
 						const file = item.getAsFile();
 						if (file) {
 							console.log(`… file[${i}].name = ${file.name}`);
-							sendFileToImgur(file);
+							const input = document.getElementById("img") as HTMLInputElement;
+							input.files = [file];
 						}
 					}
 				});
 			} else {
 				// Use DataTransfer interface to access the file(s)
-				[...(ev as DragEvent).dataTransfer.files].forEach((file, i) => {
+				[...ev.dataTransfer.files].forEach((file, i) => {
 					console.log(` file[${i}].name = ${file.name}`);
+					const input = document.getElementById("img") as HTMLInputElement;
+					input.files = [file];
 				});
 			}
 		}
@@ -66,8 +37,47 @@
 		// Prevent default behavior (Prevent file from being opened)
 		ev.preventDefault();
 	};
-</script>
 
+	const submitHandler = async (ev: Event) => {
+		ev.preventDefault();
+		const formData = new FormData();
+		const file = (document.getElementById("img") as HTMLInputElement).files![0];
+
+		
+		formData.append("img", file);
+		const response = await fetch("/uploadPhoto?/upload", {
+			method: "POST",
+			body: formData
+		});
+		const result = await response.json();
+		console.log(result);
+	};
+
+
+	// const submitHandler = async (ev: Event) => {
+	// 	ev.preventDefault();
+
+	// 	const file = (document.getElementById('img') as HTMLInputElement).files![0];
+
+	// 	const reader = new FileReader();
+	// 	reader.readAsDataURL(file);
+	// 	reader.onload = async function () {
+	// 		if (typeof reader.result === 'string') {
+	// 			const base64Data = reader.result!.split(',')[1];
+	// 			const formData = new FormData();
+	// 			formData.append('img', base64Data);
+
+	// 			console.log(base64Data);
+	// 			const response = await fetch('/uploadPhoto?/upload', {
+	// 				method: 'POST',
+	// 				body: formData
+	// 			});
+	// 			const result = await response.json();
+	// 			console.log(result);
+	// 		}
+	// 	};
+	// };
+</script>
 <div class="center-items" style="top:10%;position:absolute">
 	<h1>Upload Photo</h1>
 	<br />
@@ -78,12 +88,12 @@
 	</p>
 
 	<br />
-	<div id="form" style="top: 15%;width:auto;height:max-content;">
+	<div id="form"  style="top: 15%;width:auto;height:max-content;">
 		<div id="drop_zone" on:drop={dropHandler} on:dragover={dragOverHandler}>
 			<label for="img">Select image:</label>
 			<input type="file" id="img" name="img" accept="image/*" />
 			<div style="padding-left:80%">
-				<Button color="primary" on:click={() => dropHandler(event)}>Submit</Button>
+				<Button color="primary" on:click={submitHandler}>Submit</Button>
 			</div>
 
 			<p>Drag one file to this <i>drop zone</i>.</p>
